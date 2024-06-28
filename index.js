91439ff184e1020c366d8dca6709ea7513f2cc5b
Winnie-Flora <contentcreatorwinnie@gmail.com>
2024-06-28 19:34:53 +0300

    Adds scope lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName='bob';
+
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+    console.log(setBestCustomer);
+    return (bestCustomer='not bob');
+}
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob';
+}
+
+const leastFavoriteCustomer='Dan';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='Lin';
+}
+
